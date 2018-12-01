9ed69b4a8b49e44a3c2f4d82fdc4584da097ece8
Samuel <samuel.band@hotmail.com>
2018-12-01 20:52:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+const handlebars = require('express-handlebars').create({defaultLayout: 'main'})
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+const port = 8080
+
+app.get('/', (req,res) => {
+	res.render('home')
+})
+
+
+app.get('/page1', (req,res) => {
+	res.render('page1')
+})
+
+
+app.get('/about', (req,res) => {
+	res.render('about')
+})
+
+app.listen(port, () => console.log(`app listening on port ${port}`))
